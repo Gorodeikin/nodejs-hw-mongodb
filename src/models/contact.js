04b73172ecc5e03c0bb4626cbfc6315b536dbbc2
Sergii Gorodeikin <55316965+iEnchanter@users.noreply.github.com>
2025-10-04 02:09:42 +0200

    fix: remove __v from Contact schema

--- src/models/contact.js
+++ src/models/contact.js
@@ -15,7 +15,10 @@ const contactSchema = new mongoose.Schema(
       required: true,
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
 
 export const Contact = mongoose.model("Contact", contactSchema);
